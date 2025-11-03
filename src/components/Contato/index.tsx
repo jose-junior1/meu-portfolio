2dab8357bfbd3dcf9a0ce4f1b9615adf510780df
José Junior <juhsantos.goleiro634@gmail.com>
2025-11-03 14:34:58 -0300

    feat(contato): implementa formulario para contato e selecao de formas de contato a partir de estado

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -1,41 +1,108 @@
-import { ContainerContato, Content } from "./styles"
+import * as S from "./styles"
 import wppIcon from "../../assets/img/icons/wpp.png"
 import mailIcon from "../../assets/img/icons/mail.png"
+import { useState } from "react"
 
 const Contato = () => {
+    const [activeView, setActiveView] = useState<'form' | 'cards'>('form')
+
+    const selectForm = () => {
+        setActiveView('form')
+    }
+
+    const selectCards = () => {
+        setActiveView('cards')
+    }
+
+
     return (
-        <ContainerContato id="contato">
+        <S.ContainerContato id="contato">
             <h2>Vamos <span>conversar?</span></h2>
-            <Content>
-                <a
-                    href="https://wa.me/5511996411760"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    title="Conversar no Whatsapp (11)99641-1760"
-                >
-                    <img src={wppIcon} alt="WhatsApp" />
-                    (11) 99641-1760
-                </a>
-                <a
-                    href="https://wa.me/5511995937268"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    title="Conversar no Whatsapp (11)99593-7268"
-                >
-                    <img src={wppIcon} alt="WhatsApp" />
-                    (11) 99593-7268
-                </a>
-                <a
-                    href="mailto:jose.j.j.silva634@gmail.com"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    title="Enviar email para jose.j.j.silva634@gmail.com"
-                >
-                    <img src={mailIcon} alt="WhatsApp" />
-                    jose.j.j.silva634@gmail.com
-                </a>
-            </Content>
-        </ContainerContato>
+            <S.SelectFormContact>
+                <p>
+                    Escolha uma forma de contato
+                </p>
+                <div>
+                    <button
+                        className={activeView === 'form' ? 'is-active' : undefined}
+                        type="button"
+                        onClick={selectForm}
+                        title="Clique para selecionar formulário"
+                    >
+                        Formulário
+                    </button>
+                    <button
+                        className={activeView === 'cards' ? 'is-active' : undefined}
+                        type="button"
+                        onClick={selectCards}
+                        title="Clique para selecionar cartões"
+                    >
+                        Cartões
+                    </button>
+                </div>
+            </S.SelectFormContact>
+            {activeView === 'form' ? (
+                <S.FormContact>
+                    <S.BlockContact>
+                        <label htmlFor="name">Nome *</label>
+                        <input
+                            id="name"
+                            type="text"
+                            placeholder="Insira seu nome"
+                            required
+                        />
+                    </S.BlockContact>
+                    <S.BlockContact>
+                        <label htmlFor="email">E-mail *</label>
+                        <input
+                            id="email"
+                            type="email"
+                            placeholder="Insira seu e-mail ex.: exemplo@gmail.com"
+                            required
+                        />
+                    </S.BlockContact>
+                    <S.BlockContact>
+                        <label htmlFor="text-message">Mensagem*</label>
+                        <textarea
+                            id="text-message"
+                            placeholder="Insira sua mensagem aqui"
+                            required
+                        />
+                    </S.BlockContact>
+                    <button type="submit">Enviar</button>
+                </S.FormContact>
+            ) : (
+                <S.Content>
+                    <a
+                        href="https://wa.me/5511996411760"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        title="Conversar no Whatsapp (11)99641-1760"
+                    >
+                        <img src={wppIcon} alt="WhatsApp" />
+                        (11) 99641-1760
+                    </a>
+                    <a
+                        href="https://wa.me/5511995937268"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        title="Conversar no Whatsapp (11)99593-7268"
+                    >
+                        <img src={wppIcon} alt="WhatsApp" />
+                        (11) 99593-7268
+                    </a>
+                    <a
+                        href="mailto:jose.j.j.silva634@gmail.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        title="Enviar email para jose.j.j.silva634@gmail.com"
+                    >
+                        <img src={mailIcon} alt="WhatsApp" />
+                        jose.j.j.silva634@gmail.com
+                    </a>
+                </S.Content>
+            )}
+        </S.ContainerContato>
     )
 }
 
--- src/components/Contato/styles.ts
+++ src/components/Contato/styles.ts
@@ -5,6 +5,7 @@ export const ContainerContato = styled.section`
     display: flex;
     flex-direction: column;
     justify-content: center;
+    gap: 0;
     width: 100%;
     height: 100vh;
     background-color: #1b1a1aff;
@@ -12,17 +13,135 @@ export const ContainerContato = styled.section`
     text-align: center;
 
     ${breakpoints.mobile`
-        padding: 24px;
+        padding: 0 16px;
+    `}
+
+    ${breakpoints.tablet`
+        padding: 0 16px;
     `}
 `
 
-export const Content = styled.div`
-    margin: 0 auto;
-    width: 800px;
+export const SelectFormContact = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 10px;
+    width: 100%;
+    padding-bottom: 50px;
 
-    h2 {
-        font-size: 40px;
+    p {
+        color: rgba(255, 255, 255, 0.8);
     }
+
+    div {
+        display: flex;
+        justify-content: center;
+        gap: 10px;
+
+        button {
+            padding: 10px;
+            border-radius: 8px;
+            border: none;
+            background-color: #5e5d5dff;
+            color: #f1f1f1;
+    
+            &.is-active {
+                background-color: #017786ff;
+                font-weight: bold;
+            }
+    
+        }
+    }
+
+    ${breakpoints.mobile`
+        gap: 10px;
+
+        button {
+            width: 100%;
+        }
+    `}
+`
+
+export const FormContact = styled.form`
+    max-width: 800px;
+    width: 100%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    
+    button {
+        height: 32px;
+        width: 100%;
+        border-radius: 6px;
+        background-color: #21613c;
+        color: #f1f1f1;
+        border: 1px solid transparent;
+
+        &:hover {
+            border-color: #21613c;
+            background-color: transparent;
+        }
+    }
+`
+
+export const BlockContact = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    text-align: left;
+
+    input,
+    textarea {
+        border-radius: 6px;
+        outline: none;
+    }
+
+    input:focus,
+    textarea:focus {
+        border-color: #00BCD4;
+    }
+
+    input {
+        height: 32px;
+        padding: 0 16px;
+        border: none;
+        border-bottom: 2px solid #c1c1c1;
+        background-color: #2b2929ff;
+        color: #c5c5c5;
+        font-size: 18px;
+    }
+
+    textarea {
+        max-width: 100%;
+        width: 100%;
+        field-sizing: content;
+        resize: none;
+        min-height: 80px;
+        max-height: 300px;
+        overflow-y: auto;
+        padding: 16px;
+        font-size: 18px;
+        background-color: #2b2929ff;
+        color: #c5c5c5;
+        border: 1px solid #c1c1c1;
+
+        ${breakpoints.mobile`
+            max-height: 180px;
+        `}
+
+        ${breakpoints.tablet`
+            max-height: 220px;
+        `}
+    }
+`
+
+export const Content = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
 
     a {
         position: relative;
@@ -33,9 +152,10 @@ export const Content = styled.div`
         padding: 24px;
         background-color: #242323ff;
         font-size: 18px;
-        margin: 50px auto 20px auto;
         border-radius: 8px;
-        width: 50%;
+        max-width: 800px;
+        width: 100%;
+        margin: 0 auto;
         cursor: pointer;
         transition: transform 0.3s ease-in-out;
         border: 1px solid transparent;
